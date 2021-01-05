d6a8bcb5dd47f9dab3f03b72f50484cf29178f2f
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-01-05 15:09:51 -0800

    one blog likes test works

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) return 0
+
+  if (blogs.length === 1) return blogs[0].likes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
