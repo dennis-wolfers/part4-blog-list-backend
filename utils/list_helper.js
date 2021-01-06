b3a2cd40be4d114f4793ef52c62eb2d903edca01
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-01-06 11:54:41 -0800

    implemented through 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,8 @@ const totalLikes = (blogs) => {
   if (blogs.length === 0) return 0
 
   if (blogs.length === 1) return blogs[0].likes
+
+  return blogs.reduce((likes, blog) => likes + blog.likes, 0)
 }
 
 module.exports = {
