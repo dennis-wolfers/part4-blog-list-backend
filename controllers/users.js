4dc8f2d51d375b89a5fea638d6d4d441be8c313b
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-04-02 13:55:54 -0700

    4.17 added .populate to .get routes

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response, next) => {
   try {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1, id: 1 })
     response.json(users)
   } catch (exception) {
     next(exception)
