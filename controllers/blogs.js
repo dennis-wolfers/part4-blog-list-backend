003271befe06ece505e7d64145ab6a5a8f4bcf8e
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-04-03 13:39:39 -0700

    4.17 implemented

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response, next) => {
   try {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
   } catch (exception) {
     next(exception)
@@ -12,9 +13,15 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   const blog = new Blog(request.body)
+  const user = await User.findOne()
+  blog.user = user._id
 
   try {
     const savedBlog = await blog.save()
+
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
+
     response
       .status(201)
       .json(savedBlog)
