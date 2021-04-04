3c1217470e2f5eb88799473801834e93a2cba26a
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-04-04 10:58:07 -0700

    4.18 implemented

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const blogsRouter = require('./controllers/blogs')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const loginRouter = require('./controllers/login')
 
 logger.info(`connecting to ${config.MONGODB_URI}`)
 
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
