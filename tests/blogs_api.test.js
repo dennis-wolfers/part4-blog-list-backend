9272446f9bd98cb5fc50a440c56162123b1fc901
Dennis Wolfers <dennis.wolfers@gmail.com>
2021-05-04 12:59:07 -0700

    4.23 implemented

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -54,26 +54,39 @@ describe('when initialized with some test blog posts', () => {
       .get('/api/blogs')
       .set('Authorization', 'bearer ' + token)
 
-    console.log(`response: ${response.body}`)
-
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
 
   test('id is defined', async () => {
-    const response = await api.get('/api/blogs')
+    const loggedIn = await api
+      .post('/api/login')
+      .send({ username: 'root', password: 'sekret' })
+    const token = loggedIn.body.token
+
+    const response = await api
+      .get('/api/blogs')
+      .set('Authorization', 'bearer ' + token)
 
     expect(response.body[0].id).toBeDefined()
   })
 
   describe('post tests', () => {
     test('blog is successfully added', async () => {
+      const loggedIn = await api
+        .post('/api/login')
+        .send({ username: 'root', password: 'sekret' })
+      const token = loggedIn.body.token
+
       await api
         .post('/api/blogs')
+        .set('Authorization', 'bearer ' + token)
         .send(helper.blogToAdd)
         .expect(201)
         .expect('Content-Type', /application\/json/)
 
-      const response = await api.get('/api/blogs')
+      const response = await api
+        .get('/api/blogs')
+        .set('Authorization', 'bearer ' + token)
 
       expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
 
@@ -83,6 +96,11 @@ describe('when initialized with some test blog posts', () => {
     })
 
     test('new blog missing a likes entry defaults to zero', async () => {
+      const loggedIn = await api
+        .post('/api/login')
+        .send({ username: 'root', password: 'sekret' })
+      const token = loggedIn.body.token
+
       const blogSansLikes = {
         title: 'Somebody has to like me best',
         author: 'Dianne Wiest',
@@ -91,16 +109,26 @@ describe('when initialized with some test blog posts', () => {
 
       await api
         .post('/api/blogs')
+        .set('Authorization', 'bearer ' + token)
         .send(blogSansLikes)
         .expect(201)
         .expect('Content-Type', /application\/json/)
 
-      const response = await api.get('/api/blogs')
+      const response = await api
+        .get('/api/blogs')
+        .set('Authorization', 'bearer ' + token)
+
       const addedBlog = response.body.filter(r => r.title === 'Somebody has to like me best')
+
       expect(addedBlog[0].likes).toBe(0)
     })
 
     test('new blog without Title and URL returns 400', async () => {
+      const loggedIn = await api
+        .post('/api/login')
+        .send({ username: 'root', password: 'sekret' })
+      const token = loggedIn.body.token
+
       const blogWithoutTitleOrURL = {
         title: null,
         author: 'anon',
@@ -109,6 +137,7 @@ describe('when initialized with some test blog posts', () => {
 
       await api
         .post('/api/blogs')
+        .set('Authorization', 'bearer ' + token)
         .send(blogWithoutTitleOrURL)
         .expect(400)
     })
@@ -116,12 +145,18 @@ describe('when initialized with some test blog posts', () => {
 
   describe('update of a blog', () => {
     test('succeeds with a 200 return code', async () => {
+      const loggedIn = await api
+        .post('/api/login')
+        .send({ username: 'root', password: 'sekret' })
+      const token = loggedIn.body.token
+
       const blogsAtStart = await helper.blogsInDB()
       const blogToUpdate = blogsAtStart[0]
       blogToUpdate.likes = blogToUpdate.likes + 1
 
       await api
         .put(`/api/blogs/${blogToUpdate.id}`)
+        .set('Authorization', 'bearer ' + token)
         .send(blogToUpdate)
         .expect(200)
     })
@@ -129,11 +164,17 @@ describe('when initialized with some test blog posts', () => {
 
   describe('deletion of a blog', () => {
     test('succeeds with a 204 return code', async () => {
+      const loggedIn = await api
+        .post('/api/login')
+        .send({ username: 'root', password: 'sekret' })
+      const token = loggedIn.body.token
+
       const blogsAtStart = await helper.blogsInDB()
       const blogToDelete = blogsAtStart[0]
 
       await api
         .delete(`/api/blogs/${blogToDelete.id}`)
+        .set('Authorization', 'bearer ' + token)
         .expect(204)
 
       const blogsAtEnd = await helper.blogsInDB()
